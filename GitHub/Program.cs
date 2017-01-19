ed0372d63dce65cdb7ec6363a44b5ad9f6f6bae2
B00Bam <jonas.naitmbark@elev.ga.lbs.se>
2017-01-19 14:28:37 +0100

    k

---
+++ GitHub/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello HelloWorld = new Hello();
+
+            HelloWorld.Run();
+        }
+    }
+}
